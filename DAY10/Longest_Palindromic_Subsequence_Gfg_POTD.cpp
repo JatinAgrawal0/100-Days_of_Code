874a6b8042a9b460ea8828b9e6e6f11472665e2a
Jatin Agrawal <132620426+JatinAgrawal0@users.noreply.github.com>
2023-07-19 19:49:21 +0530

    Create Longest_Palindromic_Subsequence_Gfg_POTD.cpp

---
+++ DAY10/Longest_Palindromic_Subsequence_Gfg_POTD.cpp
@@ -0,0 +1,38 @@
+//GFG POTD---------------------------19/07/2023
+
+class Solution{
+  public:
+    vector<vector<int>> dp;
+
+    int solve(int left, int right, string a) 
+    {
+       if(left == right)
+        return 1;
+       
+       if(left>right)
+        return 0;
+       
+       if(dp[left][right]!=-1)
+        return dp[left][right];
+       
+       if(a[left]==a[right])
+       {
+           dp[left][right] = 2+solve(left+1,right-1,a);
+       }
+       else
+       {
+           dp[left][right] = max(solve(left+1,right,a),solve(left,right-1,a));
+       }
+       
+       return dp[left][right];
+    }
+    
+    int longestPalinSubseq(string A) 
+    {
+        //code here
+        int n = A.size();
+        dp.clear();
+        dp.resize(n, vector<int>(n, -1));
+        return solve(0, n - 1, A);
+    }
+};
