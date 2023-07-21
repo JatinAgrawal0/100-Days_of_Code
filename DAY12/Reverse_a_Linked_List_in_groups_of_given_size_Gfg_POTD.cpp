328dc44c391f73b71051b47a313e23438cd47108
Jatin Agrawal <132620426+JatinAgrawal0@users.noreply.github.com>
2023-07-21 21:42:02 +0530

    Create Reverse_a_Linked_List_in_groups_of_given_size_Gfg_POTD.cpp

---
+++ DAY12/Reverse_a_Linked_List_in_groups_of_given_size_Gfg_POTD.cpp
@@ -0,0 +1,46 @@
+//GFG POTD--------------------------------------21/07/2023
+
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        // Complete this method
+    struct node * curr=head;
+    struct node * prev=NULL;
+    struct node * next=NULL;
+    int count=0;
+    while(curr!=NULL&&count<k)
+    {
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+        count++;
+    }
+    
+    if(next!=NULL)
+    {
+        head->next=reverse(next,k);
+    }
+    return prev;
+    }
+};
