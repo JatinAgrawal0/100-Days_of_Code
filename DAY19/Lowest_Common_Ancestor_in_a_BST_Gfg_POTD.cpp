61b272066b72bc1f59074f6a7f7b8c3d17dbe75b
Jatin Agrawal <132620426+JatinAgrawal0@users.noreply.github.com>
2023-07-28 22:27:52 +0530

    Create Lowest_Common_Ancestor_in_a_BST_Gfg_POTD.cpp

---
+++ DAY19/Lowest_Common_Ancestor_in_a_BST_Gfg_POTD.cpp
@@ -0,0 +1,19 @@
+//GFG POTD----------------------------------28/07/2023
+
+//Function to find the lowest common ancestor in a BST.
+class Solution{
+    public:
+        Node* LCA(Node *root, int n1, int n2)
+        {
+            // code here
+            if(root == NULL  || root->data  == n1 || root->data ==n2) return root;
+            
+            Node * left = LCA(root->left,n1,n2);
+            Node * right = LCA(root->right,n1,n2);
+            
+            if(left == NULL) return right;
+            else if(right == NULL) return left;
+            else return root;
+        }
+
+};
