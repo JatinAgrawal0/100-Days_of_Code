fd56e83845f57783b8eac2a3eb10d39fe1cefe40
Jatin Agrawal <132620426+JatinAgrawal0@users.noreply.github.com>
2023-07-29 23:06:06 +0530

    Create Median_of_BST_Gfg_POTD.cpp

---
+++ DAY20/Median_of_BST_Gfg_POTD.cpp
@@ -0,0 +1,44 @@
+//GFG POTD------------------------------------29/07/2023
+
+
+
+/*
+Structure of the binary Search Tree is as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+// your task is to complete the Function
+// Function should return median of the BST
+
+void inorder(struct Node* root,vector<float> &vec){
+    if(root==NULL){
+        return ;
+    }
+    inorder(root->left,vec);
+    vec.push_back(root->data);
+    inorder(root->right,vec);
+}
+
+
+float findMedian(struct Node *root)
+{
+      //Code here
+      vector<float> vec;
+      inorder(root,vec);
+      int n=vec.size();
+      if(n%2==0){
+           return ((vec[n/2]+vec[n/2 -1])/2);
+          
+      }
+      else{
+          return vec[n/2];
+}
+}
