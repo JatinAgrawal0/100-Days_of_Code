e6d45bc4e5e25ad8950f22b75fb1a5d28d28f60d
Jatin Agrawal <132620426+JatinAgrawal0@users.noreply.github.com>
2023-08-21 23:08:38 +0530

    Create Number_of_occurrence_Gfg_POTD.cpp

---
+++ DAY42/Number_of_occurrence_Gfg_POTD.cpp
@@ -0,0 +1,18 @@
+//GFG POTD---------------------------------------------------21/08/2023
+//Number of occurrence
+
+//User function template for C++
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int count(int arr[], int n, int x) {
+	    // code here
+	    int first = lower_bound(arr,arr+n,x)-arr;
+	    int last = (upper_bound(arr,arr+n,x)-arr)-1;
+	    
+	    if(first < n && arr[first] == x)
+	        return last-first+1;
+	    return 0;
+	}
+};
