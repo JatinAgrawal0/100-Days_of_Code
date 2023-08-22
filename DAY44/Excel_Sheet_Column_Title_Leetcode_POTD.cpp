d2db33d29f3fbddc699ee2f37fe956366d528756
Jatin Agrawal <132620426+JatinAgrawal0@users.noreply.github.com>
2023-08-22 23:08:11 +0530

    Create Excel_Sheet_Column_Title_Leetcode_POTD.cpp

---
+++ DAY44/Excel_Sheet_Column_Title_Leetcode_POTD.cpp
@@ -0,0 +1,14 @@
+/LEETCODE POTD---------------------------------------------------22/08/2023
+//Excel Sheet Column Title
+
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string out;
+        while (columnNumber > 0) {
+            out = char('A' + (columnNumber - 1) % 26) + out;
+            columnNumber = (columnNumber - 1) / 26;
+        }
+        return out;
+    }
+};
