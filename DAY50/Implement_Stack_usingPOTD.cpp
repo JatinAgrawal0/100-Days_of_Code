439f3fae508fe0f7bc57630ba0b4706f2fb125bf
Jatin Agrawal <132620426+JatinAgrawal0@users.noreply.github.com>
2023-10-18 10:24:16 +0530

    Create Implement_Stack_usingPOTD.cpp

---
+++ DAY50/Implement_Stack_usingPOTD.cpp
@@ -0,0 +1,43 @@
+//LEETCDOE POTD-----------------------------------------28/08/2023
+//Implement Stack using Queues
+
+class MyStack {
+private:
+       std::queue<int> q;
+public:
+    MyStack() {
+        
+    }
+    
+
+    void push(int x) {
+        q.push(x);
+        for (int i = 0; i < q.size() - 1; ++i) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int val = q.front();
+        q.pop();
+        return val;
+    }
+    
+    int top() {
+       return q.front();  
+    }
+    
+    bool empty() {
+         return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
